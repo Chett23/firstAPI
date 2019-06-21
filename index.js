c8c0929887fb07d3fedb1b1bb4ffe7650bdcc07b
Chester Hansen <chester@Chesters-MacBook-Pro.local>
2019-06-20 20:52:31 -0600

    added static ui build folder

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const jwt = require('jsonwebtoken')
 const monk = require('monk')
 
 
-const port = process.env.PORT || 5001
-const url = process.env.DB_URL || "mongodb://admin:admin@chesterfirstdb-shard-00-00-i7cmi.mongodb.net:27017,chesterfirstdb-shard-00-01-i7cmi.mongodb.net:27017,chesterfirstdb-shard-00-02-i7cmi.mongodb.net:27017/StoreDB?ssl=true&replicaSet=ChesterFirstDB-shard-0&authSource=admin&retryWrites=true"
+const port = process.env.PORT || 5000
+const url = process.env.DB_URL
 const db = monk(url)
 db.then(() => {
   console.log('connected')
@@ -18,14 +18,11 @@ const userCart = db.get('Cart')
 const users = db.get('Users')
 const secret = 'shhh, dont tell anyone!'
 
-
+app.use(express.static('ui_build'))
 app.use(require('./headers'))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-  res.send('success')
-})
 
 app.get('/items', (req, res) => {
   userItems.find({})
@@ -93,7 +90,7 @@ app.delete('/cart/:_id', (req, res) => {
 })
 
 app.post('logout', (req, res) => {
-  
+
 })
 
 app.post('/login', (req, res) => {
@@ -117,8 +114,8 @@ app.post('/login', (req, res) => {
         console.log(err)
         res.status(401).send("login Failed" + err)
       })
-    } else {
-      users.findOne({ userName: req.body.userName, password: req.body.password })
+  } else {
+    users.findOne({ userName: req.body.userName, password: req.body.password })
       .then(user => {
         let { password, ...userResponse } = user
         let token = jwt.sign(userResponse, secret)
@@ -134,92 +131,5 @@ app.post('/login', (req, res) => {
 
 app.listen(port, (err) => {
   if (err) { throw err }
-  console.log('Server up and running on port 5000')
+  console.log(`Server up and running on port ${port}`)
 })
-
-
-
-
-// let userItems = [
-//   {
-//     name: 'Fire Place',
-//     image: 'https://images-na.ssl-images-amazon.com/images/I/410gOhZjUSL._SY300_QL70_.jpg',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'iPad',
-//     image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/pa/ipad/pro/ipad-pro-12-11-select-201810_FMT_WHH?wid=2000&amp;hei=2000&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1540576009788',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'iPhone',
-//     image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/select/iphone7-select-2019-family?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1550795429263',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Chicken Nuggets',
-//     image: 'https://addapinch.com/wp-content/uploads/2011/09/Recipes-Chicken-Nuggets-14-440x600-440x270.jpg',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Apples',
-//     image: 'https://www.specialtyproduce.com/sppics/193.png',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Ice Cream',
-//     image: 'https://static01.nyt.com/images/2016/05/24/dining/24COOKING-BANANA-ICE-CREAM1/24COOKING-BANANA-ICE-CREAM1-threeByTwoMediumAt2X-v2.jpg',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Soda',
-//     image: 'https://cf.ltkcdn.net/best/images/std/182338-425x283-best-selling-sodas.jpg',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Shoes',
-//     image: 'https://cdn.shopify.com/s/files/1/1104/4168/products/Allbirds_W_Wool_Runner_Kotare_GREY_ANGLE_0f3bfe63-ac7d-4106-9acf-d26f8414ac53_600x600.png?v=1542064004',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Shirt',
-//     image: 'https://static5.cilory.com/288824-large_default/nologo-off-white-red-casual-shirt.jpg',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Coat',
-//     image: 'https://cdn.shopify.com/s/files/1/0092/8553/3755/products/product-image-550363401_543x_91801403-ec14-4e18-abe9-e9681fc57cca_394x.jpg?v=1547408378',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Jacket',
-//     image: 'https://www.revzilla.com/product_images/0091/7906/scorpion1909_leather_jacket_black_750x750.jpg',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Boots',
-//     image: 'https://dsw.scene7.com/is/image/DSWShoes/317277_200_ss_01?$pdp-image$',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Books',
-//     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQae-1m0UIpCeH9Khulq2qfvj0LRj6z1gkv-uosegTavbyKezZeUw',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Jewlery',
-//     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBses_-yvsM5vQiZsWmbI2jqDaVZdQZp17ysUyQrmRFR6R4PmD6w',
-//     price: 3.99,
-//   },
-//   {
-//     name: 'Knowledge',
-//     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtLmcBL-Sd8FEHM0DoBRf88iWoOl-urO_spnoAJrLX6X68JIbn-g',
-//     price: 3.99,
-//   }
-// ]
-// let userCart = []
-//
-//
-// for (let i in userItems) {
-//   userItems[i].id = randomstring.generate(7)
-// }
---
+++ ui_build/precache-manifest.302aa8ec6498846c0a73df4fd24e63e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "66a4d53e104ddda31da0",
+    "url": "/static/js/main.86d67c5a.chunk.js"
+  },
+  {
+    "revision": "84ac5fd50539d0ab2aad",
+    "url": "/static/js/2.87fb154d.chunk.js"
+  },
+  {
+    "revision": "66a4d53e104ddda31da0",
+    "url": "/static/css/main.049d8d0f.chunk.css"
+  },
+  {
+    "revision": "8e585bf09de30dc6e625eee30281c130",
+    "url": "/index.html"
+  }
+];
